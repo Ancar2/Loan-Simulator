3e24551b99bac8a6763e6b0976f8eed9c436eeac
JohanCard <johancardgit@gmail.com>
2025-11-21 16:51:14 -0500

    se modifico el index.js para usar CORS

--- index.js
+++ index.js
@@ -2,21 +2,34 @@ const express = require('express');
 const connectionDB = require('./config/db');
 const cookieParser = require('cookie-parser');
 const router = require('./routes/route');
-
+const cors = require('cors'); 
 require('dotenv').config();
 
-
 const app = express();
-app.use(express.json()); 
+
+// Configuración de CORS
+app.use(cors({
+  origin: 'http://localhost:4200', // tu front-end Angular
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true // si vas a usar cookies o auth
+}));
+
+
+// Middlewares
+app.use(express.json());
 app.use(cookieParser());
-app.use('/api',router)
+
+// Rutas
+app.use('/api', router);
+
 app.use('/api/health', (req, res) => {
-  res.status(200).json({msj: 'API "simulator" is healthy'});
+  res.status(200).json({ msj: 'API "simulator" is healthy' });
 });
 
+// Conexión a DB
 connectionDB();
 
-app.listen (process.env.PORT, () => {
+// Servidor
+app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
 });
-
